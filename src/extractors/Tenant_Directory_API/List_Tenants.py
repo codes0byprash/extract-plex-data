7f98f316bec427d03e3ccd2fdd227dcce5b19c3e
Pratik <Pdhakal@pmsquare.com>
2025-07-16 08:12:38 +0545

    feat: updated supplier api and tenant directory api

--- src/extractors/Tenant_Directory_API/List_Tenants.py
+++ src/extractors/Tenant_Directory_API/List_Tenants.py
@@ -1,4 +1,9 @@
-from src.s3uploader.upload_to_s3 import fetch_and_upload_if_changed
-def fetch_and_upload(api_url,cos_filename):
+# from src.s3uploader.upload_to_s3 import fetch_and_upload_if_changed
+# def fetch_and_upload(api_url,cos_filename):
+#     """ Always fetch and upload"""
+#     return fetch_and_upload_if_changed(api_url,cos_filename)
+from src.s3uploader.upload_to_s3 import call_plex_api
+def tenant_directory(headers,url):
     """ Always fetch and upload"""
-    return fetch_and_upload_if_changed(api_url,cos_filename)
+    cos_filename = "Tenant_Directory"  # or however you want to name it
+    return call_plex_api(url,cos_filename)
